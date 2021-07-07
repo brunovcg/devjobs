0a0a3df52142b9049891485d52c1d6f50e888dee
Julia Clepf <jumgc09@gmail.com>
2021-07-07 16:05:52 -0300

    feat: create button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+const Button = ({children, setColor, setSize, setClick}) => {
+  return(
+    <button
+        setColor={setColor}
+        onClick={setClick}
+        setSize={setSize}
+      >{children}
+    </button>
+  )
+}
+export default Button
