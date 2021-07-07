d86c60e583554b20e6aa6ba29640f65cbb0f3ced
Thiago Henrique Dias Silva <professorthiagohenriqueds@gmail.com>
2021-07-07 15:34:03 -0300

    fix: Changed component name to route.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch } from "react-router-dom";
-import Route from "./routes";
+import Route from "./route";
 // import Home from '../pages/Home'
 
 const Routes = () => {
