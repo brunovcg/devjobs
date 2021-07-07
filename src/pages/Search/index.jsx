46de0a5fa8cec55382d88b5f110c91b3e4425f48
Carlos Martorini <c7.silveira@gmail.com>
2021-07-07 16:52:40 -0300

    style: create container search, search bar and container cards styled components

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,17 +1,21 @@
+import { ContainerSearch, SearchBar, ContainerCards } from './styles';
+
 const Search = () => {
     return (
         <>
             <p>Header - another feature to be developed</p>
-            <div>
-                <input placeholder='Techs'/>
-                <input placeholder='Specialization'/>
-                <input placeholder='Seniority'/>
-                <input placeholder='Disponibility'/>
-                <button>Search</button>
-            </div>
-            <div>
+            <ContainerSearch>
+                <SearchBar>
+                    <input placeholder='Techs'/>
+                    <input placeholder='Specialization'/>
+                    <input placeholder='Seniority'/>
+                    <input placeholder='Disponibility'/>
+                    <button>Search</button>
+                </SearchBar>
+            </ContainerSearch>
+            <ContainerCards>
                 <p>Cards Users - another feature to be developed</p>
-            </div>
+            </ContainerCards>
         </>
     )
 }
