de456e784dd1fb894763d4c1ecc79db70c5e65c0
Thiago Henrique Dias Silva <professorthiagohenriqueds@gmail.com>
2021-07-09 13:27:42 -0300

    feat: Implemented component route in routes.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,28 +1,21 @@
-import { Route, Switch } from "react-router-dom";
-// import Route from "./route";
+import { Switch } from "react-router-dom";
+import Route from "./route";
 import { Home } from "../pages/Home";
 import Search from "../pages/Search";
 import Dashboard from "../pages/Dashboard";
-import PageNotFound from '../pages/PageNotFound'
+import PageNotFound from '../pages/PageNotFound';
+import userLogin from '../pages/userLogin';
+import Register from '../pages/UserRegister';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/search">
-        <Search />
-      </Route>
-      <Route path="/dashboard">
-        <Dashboard />
-      </Route>
-      <Route>
-        <PageNotFound/>
-      </Route>
-
-
-      {/* <Route path="/" exact component={Home} /> */}
+      <Route path="/" exact component={Home} />      
+      <Route path="/search" exact component={Search} />
+      <Route path="/login" exact component={userLogin} />
+      <Route path="/register" exact component={Register} />      
+      <Route path="/dashboard" exact component={Dashboard} />      
+      <Route isPrivate={true} component={PageNotFound} />      
     </Switch>
   );
 };
