af0724e1eb8eb538b6c75e8811d4ff181042a415
Carlos Martorini <c7.silveira@gmail.com>
2021-07-09 06:51:14 -0300

    feat: implement new components

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,16 @@
 import { Route, Switch } from "react-router-dom";
 // import Route from "./route";
 import { Home } from "../pages/Home";
+import Search from "../pages/Search";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
 
       {/* <Route path="/" exact component={Home} /> */}
