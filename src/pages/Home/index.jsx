34b13cbdf7b8fb45ce3bc25b86b3390d1aee5cb3
Thiago Henrique Dias Silva <professorthiagohenriqueds@gmail.com>
2021-07-12 16:22:55 -0300

    feat: Companies link in home implemented.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
   return (
     <>
       <Header
-        setLeft={<HeaderLink setName="For Companies" setPath="/" />}
+        setLeft={<HeaderLink setName="For Companies" setPath="/companies" />}
         setRight={<HeaderLink setName="For Devs" setPath="/login"/>}
       />
 
