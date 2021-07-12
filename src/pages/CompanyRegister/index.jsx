ff74d6a7cf5527f75f3f0f036032412314840444
Thiago Henrique Dias Silva <professorthiagohenriqueds@gmail.com>
2021-07-12 15:23:24 -0300

    fix: Corrected confirm password field name, translated error messages, translated toastify.

--- src/pages/CompanyRegister/index.jsx
+++ src/pages/CompanyRegister/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FormStyled, Page, Text } from './styles';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import api from '../../services/api';
-
-import { useHistory, Link } from 'react-router-dom';
 
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -15,8 +14,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import Header from '../../components/Header';
 
 const CompanyRegister = () => {
-  const PasswordStrength = (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/);
-
   const schema = yup.object().shape({
     companyName: yup
         .string()
@@ -28,8 +25,7 @@ const CompanyRegister = () => {
     password: yup
         .string()
         .min(6, 'Minimum 6 digits')
-        .required('Required field')
-        .matches(PasswordStrength, 'Weak password, use lowercase, uppercase, numbers and symbols.'),
+        .required('Required field'),
     confirmPassword: yup
         .string()
         .required('Required field')
@@ -43,21 +39,22 @@ const CompanyRegister = () => {
   });
 
   const onSubmitFunction = ({ companyName, email, password }) => {
-    const company = { companyName, email, password };
+    const company = { companyName, email, password }
 
     api
       .post('/companies', company)
-      .then((response) => {
-          const { newCompany } = response.data;
+      .then((response)=> {
+        toast.success('Account created successfully'); 
 
-          localStorage.setItem('@DevJobs:Company', JSON.stringify(newCompany));
+        const newCompany = response.data;
+        localStorage.setItem('@DevJobs:Company', JSON.stringify(newCompany));
 
-          return history.push('/search')
-      })
-      .catch(err => {
-          toast.error('Invalid e-mail or password.'); 
-          console.log(err);
-      })
+        return history.push('/search')
+    })
+    .catch((err) => {
+        toast.error('An error has occurred, try again using a different e-mail.'); 
+        console.log(err)
+    });
   }
 
 
@@ -99,8 +96,8 @@ const CompanyRegister = () => {
       placeholder='Confirm Password' 
       type='password' 
       register={register}
-      name='passwordConfirm'
-      error={errors.passwordConfirm?.message}
+      name='confirmPassword'
+      error={errors.confirmPassword?.message}
       setHeight='70px'
       setWidth='100%' 
     />
@@ -114,7 +111,7 @@ const CompanyRegister = () => {
     </Button>           
   </FormStyled>
 
-  <Text>If you already have an account, <Link to='/'>sign in here.</Link></Text>
+  <Text>If you already have an account, <Link to='/company'>sign in here.</Link></Text>
   </Page>
   </>    
   );
