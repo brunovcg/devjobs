f0f602d11828dc4bed2d445717b59c765ffbbeeb
Julia Clepf <jumgc09@gmail.com>
2021-07-13 14:23:18 -0300

    feat: component button test working

--- src/tests/TestButton/testButton.test.jsx
+++ src/tests/TestButton/testButton.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Button from "../components/Button";
+import Button from "../../components/Button";
 test("basic snapshot with some optional props", () => {
   const component = renderer.create(
     <Button
