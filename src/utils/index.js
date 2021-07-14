1a288914f22e3a11e0bbefa1d01b99384434d19f
Carlos Martorini <c7.silveira@gmail.com>
2021-07-14 11:52:55 -0300

    feat: added utils languages to use for filter on search

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,22 @@ export const seniority = ['Junior', 'Middle', 'Senior'];
 export const specialization = ['Front-end', 'Back-end', 'DevOps', 'Mobile', 'Full-stack'];
 
 export const levelSkills = ['Beginner', 'Intermediate', 'Advanced'];
+
+export const languages = [
+    "JavaScript",
+    "HTML / CSS",
+    "React",
+    "TypeScript",
+    "Angular",
+    "Python",
+    "PHP",
+    "Pearl",
+    "Haskell",
+    "Kotlin",
+    "SQL",
+    "Lua",
+    "Rust",
+    "Ruby",
+    "Swift",
+    "Java"
+];
